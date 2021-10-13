62a626df9866a0d9b20d9f6ea33520ee54f20a78
Süleyman Furkan Binay <51470064+sfbinay@users.noreply.github.com>
2021-10-13 12:27:28 +0300

    Update Service.tsx

--- src/Service.tsx
+++ src/Service.tsx
@@ -9,7 +9,7 @@ const ref = "2490c8c443178a9a4d10b6a2cd541e19"
 export function getForm() {
     const params = {
         ref: ref,
-        formId:"39" /* hazir formlar = formId:"44" formId:"39" */
+        formId:"39" /* hazir formlar = formId:"44" formId:"39" formId:"56" */
     }
 
     return service.get("/forms.php", { params: params })
